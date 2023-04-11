b71c600caf692457da3ce07c7324aa72a530d7b5
Mr.PAPI <thesnakewitcher@gmail.com>
2023-04-10 21:26:02 -0400

    renamed app name in server.go module

--- internal/http/server.go
+++ internal/http/server.go
@@ -1,7 +1,7 @@
 package http
 
 import (
-	"github.com/TheSnakeWitcher/petplanet/pets"
+	"github.com/TheSnakeWitcher/PetPark/pets"
 	"github.com/labstack/echo/v4"
 )
 
@@ -14,12 +14,4 @@ func NewServer(svc pets.Service) (srv *Server) {
     srv = &Server { e }
     srv.InitRoutes(svc)
     return srv
-}
-
-func (self *Server) InitRoutes(svc pets.Service) {
-    self.GET("/hello",Home)
-    self.GET("/pets/",ListPets(svc))
-    self.GET("/pets/:id",GetPet(svc))
-    self.POST("/pets",AddPet(svc))
-    self.DELETE("/pets/:id",DelPet(svc))
 }
