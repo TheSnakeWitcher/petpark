79b81072629ef3a6563586d5db394f203f34b234
Mr.PAPI <thesnakewitcher@gmail.com>
2023-04-10 21:20:14 -0400

    order listed pets by picked data and insert query operate on not null fields

--- db/queries/pets.sql
+++ db/queries/pets.sql
@@ -2,16 +2,11 @@
 select * from pets where id = $1 ;
 
 -- name: ListPets :many
-select * from pets order by name;
+select * from pets order by picked ;
 
 -- name: AddPet :execresult
-insert into pets ( id , name, picked ,location,data) 
-values( $1,$2,now(),$3,'{}') ;
+insert into pets (id , picked , address , contact, details)
+values(DEFAULT, $1 , $2 , $3 , $4) ;
 
 -- name: DelPet :exec
 delete from pets where id = $1 ;
-
--- name: UpdateState :exec
-update pets 
-set status = 'adopted'
-where id = ?;
