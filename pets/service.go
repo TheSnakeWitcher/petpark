52b30b5458b59572ece6e85875bc93280056e0fd
Mr.PAPI <thesnakewitcher@gmail.com>
2023-04-10 21:21:47 -0400

    add service to generated go code

--- pets/service.go
+++ pets/service.go
@@ -3,7 +3,8 @@ package pets
 import (
 	"context"
 	"database/sql"
-	"math/rand"
+
+	"github.com/google/uuid"
 )
 
 type Service struct { *Queries }
@@ -12,34 +13,28 @@ func NewService(db *sql.DB) *Service {
     return &Service{ New(db) }
 }
 
-func (self Service) ListPets(ctx context.Context) ([]Pet , error) {
-    pets,err := self.Queries.ListPets(ctx)
+func (svc Service) ListPets(ctx context.Context) ([]Pet , error) {
+    pets,err := svc.Queries.ListPets(ctx)
     if err != nil {
         return []Pet{} , err
     }
-
     return pets,nil
 }
 
-func (self Service) GetPet(ctx context.Context,id int32) (Pet,error) {
-    pet , err := self.Queries.GetPet(ctx,id)
+func (svc Service) GetPet(ctx context.Context,id string) (Pet,error) {
+    pet , err := svc.Queries.GetPet(ctx,uuid.MustParse(id))
     if err != nil {
         return Pet{},err
     }
     return pet,nil
 }   
 
-func (self Service) AddPet(ctx context.Context,name,loc string) (sql.Result,error) {
-    arg := AddPetParams{
-        ID: rand.Int31(),
-        Name: sql.NullString{String: name , Valid: true},
-        Location: sql.NullString{String: loc , Valid: true},
-    }
-    pet, err := self.Queries.AddPet(ctx,arg)
-    return pet,err
+func (svc Service) AddPet(ctx context.Context,arg AddPetParams) (sql.Result,error) {
+    outPet, err := svc.Queries.AddPet(ctx,arg)
+    return outPet,err
 }
 
-func (self Service) DelPet(ctx context.Context,id int32) (error) {
-    err := self.Queries.DelPet(ctx,id)
+func (svc Service) DelPet(ctx context.Context,id string) (error) {
+    err := svc.Queries.DelPet(ctx,uuid.MustParse(id))
     return err
 }
